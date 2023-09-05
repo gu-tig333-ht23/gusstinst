6e2ee0aefebbb324242f887a6bc30ff3628dd913
Serena87 <gusstinst@student.gu.se>
2023-09-05 09:40:00 +0200

    försökt lösa emulatorproblem

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,8 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: Color.fromARGB(210, 2, 26, 37)),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
